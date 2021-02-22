451d8a1e05399a1864b019d6f878380041eac31f
cisnerosjj <jorgecisnerosgavin@gmail.com>
2021-02-22 15:45:35 +0100

    add express config

---
+++ app.js
@@ -0,0 +1,57 @@
+//Dotenv es un módulo de dependencia cero que carga variables de entorno desde un .envarchivo a process.env.
+require('dotenv').config();
+
+//El módulo de errores HTTP se utiliza para generar errores para las aplicaciones Node.js. 
+const createError = require("http-errors"); 
+
+//Requerimos el modulo express para utilizar sus funciones y variables
+const express = require('express');
+
+//Middleware que nos da más info en los logs de consola
+const logger = require('morgan');
+
+//Motor de vistas que utilizamos
+const hbs = require('hbs');
+
+
+//Requerimos routes para configurar todas nuestras rutas en un archivo
+const routes = require("./config/routes");
+
+
+
+//=================Configuracion de Express=========================
+
+//Configuramos la instacia de express
+const app = express();
+
+//Middleware para solicitudes POST que envía datos al servidor y le pide que acepte o almacene esos datos (objeto),
+//que están incluidos en el cuerpo (es decir, req.body) de esa solicitud (POST) 
+// ¡¡¡CONFLICTO SI USAMOS BODY PARSER PORQUE YA VIENE IMPLICITO!!!
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+//Configuramos carpeta public con express para poder usar css e imagenes en nuestras views
+app.use(express.static("public"));
+
+//Middleware para poder meter logs en consola
+app.use(logger("dev"));
+
+//Configuramos la ruta que contiene la vistas
+app.set("views",__dirname +"/views");
+
+//Configuramos el motor de vistas que vamos a utilizar
+app.set("view engine", "hbs");
+
+//Registramos los partials
+hbs.registerPartials(__dirname+"/views/partials");
+
+//======================================================================
+
+//Seteamos el / para poder utilizar las rutas
+app.use("/", routes);
+
+
+//=================Configuración inicialización=========================
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
