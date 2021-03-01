fccc3d03e5dcc22c25098b81806cb247c3d4197f
karuvsentor <karuvsentor@gmail.com>
2021-03-01 21:37:05 +0100

    WORKING CRUD TRACKS

--- controllers/tracks.controller.js
+++ controllers/tracks.controller.js
@@ -2,20 +2,22 @@ const mongoose = require("mongoose");
 const Track = require("../models/Track.model");
 
 module.exports.create = (req, res, next) => {
-  res.render("track/form");
+  res.render("track/trackCreate");
 };
 
 module.exports.doCreate = (req, res, next) => {
+  console.log("caca")
   function renderWithErrors(errors) {
-    res.status(400).render("routes/form", {
+    res.status(400).render("/", {
       errors: errors,
       route: req.body,
     });
   }
-
+  console.log(req.body)
   Track.create(req.body)
-    .then((u) => {
-      res.redirect(`/track/${id}`);
+    .then((track) => {
+      console.log("track",track )
+      res.redirect(`/tracks`);
     })
     .catch((e) => {
       if (e instanceof mongoose.Error.ValidationError) {
@@ -26,74 +28,18 @@ module.exports.doCreate = (req, res, next) => {
     });
 };
 
-module.exports.edit = (req, res, next) => {
-  Track.findById(req.params.id)
-    .then((track) => {
-      if (
-        !track ||
-        track.create.toString() !== req.currentUser.id.toString()
-      ) {
-        res.redirect("/");
-      } else {
-        res.render("track/form", { track });
-      }
-    })
-    .catch((e) => next(e));
-};
 
-module.exports.doEdit = (req, res, next) => {
-  function renderWithErrors(err) {
-    res.status(400).render("track/form", {
-      errors: err,
-      track: req.body,
-    });
-  }
-  Track.findById(req.params.id)
-    .then((p) => {
-      if (!p || p.author.toString() !== req.currentUser.id.toString()) {
-        res.redirect("/");
-      } else {
-        Object.entries(req.body).forEach(([k, v]) => (p[k] = v));
-        return p.save().then(() => {
-          res.redirect(`/track/${req.params.id}`);
-        });
-      }
+
+
+module.exports.tracksPage = (req, res, next) => {
+  Track.find({})
+    .then((tracks) => {
+      res.render("track/list", { tracks })
     })
     .catch((e) => {
-      if (e instanceof mongoose.Error.ValidationError) {
-        renderWithErrors(e.errors);
-      } else {
-        next(e);
-      }
-    });
-};
-
-module.exports.detail = (req, res, next) => {
-  Track.findById(req.params.id)
-    .then((track) => {
-      res.render("track/detail", {
-        track,
-        canEdit: req.currentUser
-          ? track.author.toString() === req.currentUser.id.toString()
-          : false,
-      });
+      console.log(e)
     })
-    .catch((e) => next(e));
-};
-
-module.exports.delete = (req, res, next) => {
-  Track.findOneAndDelete({ _id: req.params.id, author: req.currentUser.id })
-    .then(() => {
-      res.redirect("/");
-    })
-    .catch((e) => next(e));
-};
 
 
-
-module.exports.tracksPage = (req, res, next) =>{
-  res.render('track/list')
-  
-  
 }
 
