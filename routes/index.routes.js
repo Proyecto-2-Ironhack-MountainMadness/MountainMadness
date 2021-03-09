ef4ea3acf36e21300549ba495efce2a60a457233
cisnerosjj <jorgecisnerosgavin@gmail.com>
2021-03-09 13:28:52 +0100

    MergeMartes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -20,10 +20,8 @@ router.get('/authenticate/google/cb', usersController.doLoginGoogle);
 //=======================nodemailer-activarToken=====================
 router.get("/activate/:token",usersController.activate);
 
-<<<<<<< HEAD
 router.get("/categories/:name",categoriesController.getTracks);
  
-=======
  //========================contact us ====================//
 
 router.get("/contactus",miscController.contactus);
@@ -39,6 +37,5 @@ router.get("/aboutUs",miscController.aboutus);
 //========================terms ====================//
 router.get("/terms",miscController.terms);
 
->>>>>>> 580ad0c64c091d9f14169825e0997955b54eb147
 
 module.exports = router;
