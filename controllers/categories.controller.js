8d303049ac500037ac8af9721621f008a2fd08be
cisnerosjj <jorgecisnerosgavin@gmail.com>
2021-03-13 05:38:38 +0100

    lastlastfinalfinal

--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -1,23 +1,20 @@
 const mongoose = require("mongoose");
 const Track = require("../models/Track.model");
 const categories = require("../data/categories");
-const valoracion = require("../data/valoracion");
-const dificultad = require("../data/dificultad");
+
 
 
 module.exports.getTracks = (req, res, next) => {
     const category = req.params.name;
     console.log(category)
 
-    Track.find({categories: category},{valoracion: valoracion},{dificultad: dificultad} )
+    Track.find({categories: category} )
       .populate("author")
       .then((tracks) => {
           console.log(tracks)
         res.render("track/list", {
           tracks,
           categories: categories,
-          dificultad: dificultad,
-          valoracion: valoracion,
           isAuthor: true,
         });
       })
